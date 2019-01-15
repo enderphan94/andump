84612e744a02e2e5f44984b57e2ff3ea002e29f4
Ender Loc Phan <locpv@blockchainlabs.asia>
2019-01-15 13:20:47 +0700

    fixed code

--- andump.sh
+++ andump.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+   
+echo                                                                                              
+echo "                   .___                     "
+echo "_____    ____    __| _/_ __  _____ ______   "
+echo "\__  \  /    \  / __ |  |  \/     \\____ \  "
+echo " / __ \|   |  \/ /_/ |  |  /  Y Y  \  |_> > "
+echo "(____  /___|  /\____ |____/|__|_|  /   __/  "
+echo "     \/     \/      \/           \/|__|     "
+echo "         EnderPhan---------------------     "
+echo
+echo
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -23,6 +34,7 @@ sdcard_path+=$name
 if adb get-state 1>/dev/null 2>&1
 then
 	echo "[+] Device attached found";
+	echo
 else
 	echo "[-] No device found";
 	exit 0
@@ -35,7 +47,6 @@ then
 fi
 
 searching () {
-	echo $1
 	SAVEIFS=$IFS
 	IFS=$(echo -en "\n\b")
 	
@@ -61,7 +72,7 @@ searching () {
 	IFS=$SAVEIFS
 }
 
-$searching $sdcard_path
+#searching $sdcard_path
 searching $internal_path
 #adb shell "su -c 'strings $name'"
 
