a8b846a95e0f3de589971878aa13cf43630079d4
Ender Loc Phan <locpv@blockchainlabs.asia>
2019-01-15 11:36:17 +0700

    add more path

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # andump
 For Android Data Forensic
 
+The different data storage options available on Android:
+
+- Internal file storage: Store app-private files on the device file system.
+- External file storage: Store files on the shared external file system. This is usually for shared user files, such as photos.
+- Shared preferences: Store private primitive data in key-value pairs.
+- Databases: Store structured data in a private database.
+# Plan
+
+1. Dump data from Sandbox and external enclaves
+2. Print all sensitive data along with its file location
+3. Based on the rules file, this needs to be updated 
+
+Internal:
+1. /data/data
+
+External:
+1. /mnt/sdcard/Android/data is a softlink to /sdcard/
+2. /
+
+
+Shared Preferences
+SQLite Databases
+Realm Databases
+Internal Storage
+External Storage
+
 # Usage
 
 ```./andump.sh -p <packagename>```
--- andump.sh
+++ andump.sh
@@ -15,9 +15,10 @@ esac
 done
 set -- "${POSITIONAL[@]}"
 name=${EXTENSION}
-external_path="/data/data/"
-external_path+=$name
-database_path="$external_path/databases/"
+internal_path="/data/data/"
+internal_path+=$name
+sdcard_path="/sdcard/Android/data/"
+sdcard_path+=$name
 
 if adb get-state 1>/dev/null 2>&1
 then
@@ -32,26 +33,35 @@ then
 	echo "[-] No path supplied, please run with '-p <package name>'"
 	exit 0
 fi
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-external_folder=`adb shell "su -c 'find $external_path -type f'"`
-for file in $external_folder
-do
-	if [[ $(adb shell "su -c 'strings \"$file\"'" | head -1) == "SQLite format 3" ]];
-	then
-		for rule in "${rules[@]}"
-		do
-			#echo "$rule"
-			adb shell "su -c 'sqlite3 \"$file\" .dump | grep $rule && echo \"$file\" && echo'"
-		done
-	else
-		for rule in "${rules[@]}"
-                do
-                        #echo "$rule"
-                        adb shell "su -c 'strings \"$file\" | grep $rule && echo \"$file\" \n'"
-                done
-	fi
-done
-IFS=$SAVEIFS
+
+searching () {
+	echo $1
+	SAVEIFS=$IFS
+	IFS=$(echo -en "\n\b")
+	
+	folder=`adb shell "su -c 'find $1 -type f'"`
+
+	for file in $folder
+	do
+		if [[ $(adb shell "su -c 'strings \"$file\"'" | head -1) == "SQLite format 3" ]];
+		then
+			for rule in "${rules[@]}"
+			do
+				#echo "$rule"
+				adb shell "su -c 'sqlite3 \"$file\" .dump | grep $rule && echo \"$file\" && echo'"
+			done
+		else
+			for rule in "${rules[@]}"
+			do
+				#echo "$rule"
+				adb shell "su -c 'strings \"$file\" | grep $rule && echo \"$file\" \n'"
+			done
+		fi
+	done
+	IFS=$SAVEIFS
+}
+
+$searching $sdcard_path
+searching $internal_path
 #adb shell "su -c 'strings $name'"
 
